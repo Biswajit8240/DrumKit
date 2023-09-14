d889540f1944554eee0ce1e050028f0ac1f36209
Biswajit Dey <116413469+Biswajit8240@users.noreply.github.com>
2023-09-14 21:44:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var NumberOfDrums = document.querySelectorAll(".drum").length;
+
+function keys(value){
+  switch (value) {
+    case "w":
+      var audio = new Audio('sounds/tom-0.mp3');
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio('sounds/tom-5.mp3');
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio('sounds/tom-6.mp3');
+      audio.play();
+      break;
+    default:
+
+  }
+
+}
+
+for (var i = 0; i < NumberOfDrums; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var ButtonInnerHtml = this.innerHTML;
+    keys(ButtonInnerHtml);
+     keyAnimation(ButtonInnerHtml);
+  });
+}
+document.addEventListener("keypress",function(event){
+  keys(event.key);
+   keyAnimation(event.key);
+})
+function keyAnimation(currentKey){
+  var activeKey= document.querySelector("."+ currentKey);
+  activeKey.classList.add("pressed");
+setTimeout(function(){
+  activeKey.classList.remove("pressed");
+} ,100);
+}
